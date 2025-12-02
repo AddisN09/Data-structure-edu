cc6ce1f9c75b923d6eee0d3aa65625f1483be6dc
Addis Negash <anegash154@gmail.com>
2025-12-02 20:23:14 +0300

    feat : add simple sort to ArrayAlgorithm class

--- src/structures/Array/array.js
+++ src/structures/Array/array.js
@@ -71,4 +71,16 @@
         }
         return array;
     }
+    simpleSort(array){
+        for(let i=0;i<array.length-1;i++){
+            for(let j=i+1;j<array.length;j++){
+                if(array[j]<array[i]){
+                    let temp=array[j];
+                    array[j]=array[i];
+                    array[i]=temp;
+                }
+            }
+        }
+        return array;
+    }
  }
