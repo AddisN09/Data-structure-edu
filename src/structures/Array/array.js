85301a505d386c0d2604e67c95c7ba9a4f001757
Addis Negash <anegash154@gmail.com>
2025-12-02 14:44:02 +0300

    feat : add binary search to ArrayAlgorithm class

--- src/structures/Array/array.js
+++ src/structures/Array/array.js
@@ -19,4 +19,22 @@
         }
         return array;
     }
+     binarySearch(target,array){
+        const sortedArray=this.insertionSort(array);
+        let left=0;
+        let right=sortedArray.length-1;
+        while(left<=right){
+            let mid=Math.floor((right+left)/2);
+            if(sortedArray[mid]===target){
+                return mid;
+            }
+            else if(sortedArray[mid]>target){
+                right=mid-1;
+            }
+            else{
+                left=mid+1;
+            }
+        }
+        return -1;
+    }
  }
